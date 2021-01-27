ce996ed452f420d6176badea4389b938d7414e51
LaisMaira <36926713+LaisMaira@users.noreply.github.com>
2021-01-27 18:22:46 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+﻿function calcula(valor){
+
+	if (valor > 0 ){
+		var r = 0;
+
+		for (var i = 3; i < valor; i++) {
+
+			if ((i % 3) == 0 || (i % 5 == 0)){
+				r = parseInt(r) + parseInt(i);
+			}
+		}
+
+		$('#div').html("Resultado: "+r);
+	}else{
+		$('#div').html("<span class='erro'>Valor invalido!</span>");
+	}
+}
+
+$(document).on('keyup', function(event) {
+
+    if(event.keyCode === 13) {
+
+        $('#btncalcular').click();
+
+    }
+
+});
